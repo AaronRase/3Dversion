4def7ae85b1e57903383f81a545d7e77e9e51316
AaronRase <114154265+AaronRase@users.noreply.github.com>
2024-05-09 15:23:07 +0200

    Add files via upload

---
+++ src/include/CosmoInterface/evolvers/kernels/kernels.h
@@ -0,0 +1,8 @@
+#include "CosmoInterface/evolvers/kernels/scalarsingletkernels.h"
+#include "CosmoInterface/evolvers/kernels/gwskernels.h"
+#include "CosmoInterface/evolvers/kernels/etckernels.h" //our own contribution
+#include "CosmoInterface/evolvers/kernels/scalefactorkernels.h"
+#include "CosmoInterface/evolvers/kernels/complexscalarkernels.h"
+#include "CosmoInterface/evolvers/kernels/su2doubletkernels.h"
+#include "CosmoInterface/evolvers/kernels/u1kernels.h"
+#include "CosmoInterface/evolvers/kernels/su2kernels.h"
